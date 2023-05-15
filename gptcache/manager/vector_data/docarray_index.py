0377615324dc251fe5f8fcad781920d22fff79b1
jupyterjazz <saba.sturua@jina.ai>
2023-05-15 13:36:39 +0200

    feat: flush and close
    Signed-off-by: jupyterjazz <saba.sturua@jina.ai>

---
+++ gptcache/manager/vector_data/docarray_index.py
@@ -0,0 +1,88 @@
+import os
+from typing import List, Optional, Tuple
+
+import numpy as np
+from docarray.typing import NdArray
+from pydantic import parse_obj_as
+
+from gptcache.manager.vector_data.base import VectorBase, VectorData
+from gptcache.utils import import_in_memory_exact_search
+
+import_in_memory_exact_search()
+
+from docarray import BaseDoc, DocList  # pylint: disable=C0413
+from docarray.index import InMemoryExactNNIndex  # pylint: disable=C0413
+
+
+class DocarrayVectorData(BaseDoc):
+    """Class representing a vector data element with an ID and associated data."""
+
+    id: int
+    data: NdArray
+
+
+class DocArrayIndex(VectorBase):
+    """
+    Class representing in-memory exact nearest neighbor index for vector search.
+
+    :param index_file_path: the path to docarray index, defaults to 'docarray_index.bin'.
+    :type index_file_path: str
+    """
+
+    def __init__(self, index_file_path: str):
+        if os.path.exists(index_file_path):
+            self._index = InMemoryExactNNIndex[DocarrayVectorData](
+                index_file_path=index_file_path
+            )
+        else:
+            self._index = InMemoryExactNNIndex[DocarrayVectorData]()
+        self.index_file_path = index_file_path
+
+    def mul_add(self, datas: List[VectorData]) -> None:
+        """
+        Add multiple vector data elements to the index.
+
+        :param datas: A list of vector data elements to be added.
+        """
+        docs = DocList[DocarrayVectorData](
+            DocarrayVectorData(id=data.id, data=data.data) for data in datas
+        )
+        self._index.index(docs)
+
+    def search(
+        self, data: np.ndarray, top_k: int = -1
+    ) -> Optional[List[Tuple[float, int]]]:
+        """
+        Search for the nearest vector data elements in the index.
+
+        :param data: The query vector data.
+        :param top_k: The number of top matches to return. If -1, return all matches.
+        :return: A list of tuples, each containing the match score and the ID of the matched vector data element.
+        """
+
+        if len(self._index) == 0:
+            return None
+        query = parse_obj_as(NdArray, data)
+        docs, scores = self._index.find(query, search_field="data", limit=top_k)
+        return list(zip(scores, docs.id))
+
+    def rebuild(self, ids: Optional[List[int]] = None) -> bool:
+        """
+        In the case of DocArrayIndex, the rebuild operation is not needed.
+        """
+        return True
+
+    def delete(self, ids: Optional[List[str]]) -> None:
+        """
+        Delete the specified vector data elements from the index.
+
+        :param ids: A list of IDs of the vector data elements to be deleted.
+        """
+        if ids is not None:
+            del self._index[ids]
+
+    def flush(self) -> None:
+        self._index.persist(self.index_file_path)
+
+    def close(self) -> None:
+        self.flush()
